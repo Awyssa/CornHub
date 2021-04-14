0282171aa8b05959791918e3113767c62e85e245
awyssa <michael.ehson@gmail.com>
2021-04-14 16:44:15 +0100

    added post

--- backend/plants/views.py
+++ backend/plants/views.py
@@ -18,10 +18,24 @@ class PlantsListView(APIView):
         serialized_plants = PlantsSerializer(plants, many=True)
         return Response(serialized_plants.data, status=status.HTTP_200_OK)
 
+    def post(self, request):
+        # request.data["owner"] = request.user.id
+        plants_to_add = PlantsSerializer(data=request.data)
+        if plants_to_add.is_valid():
+            plants_to_add.save()
+            return Response(plants_to_add.data, status=status.HTTP_201_CREATED)
+        return Response(plants_to_add.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+
 class PlantsDetailView(APIView):
 
-    def get_plant(self, _request, pk):
+    def get_plant(self, pk):
         try:
             return Plants.objects.get(pk=pk)
         except Plants.DoesNotExist:
-            raise NotFound(detail="not found")
+            raise NotFound(detail="🥪 Cannot find that meal")
+
+    def get(self, _request, pk):
+        plants = self.get_plant(pk=pk)
+        serialized_meal = PlantsSerializer(plants)
+        return Response(serialized_meal.data, status=status.HTTP_200_OK)
