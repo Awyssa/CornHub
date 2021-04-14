b73ca86b7c79af3f7094b2b1f38a0cbcdb425515
awyssa <michael.ehson@gmail.com>
2021-04-14 13:22:15 +0100

    mirgratios working

---
+++ backend/jwt_auth/models.py
@@ -0,0 +1,13 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    email = models.CharField(max_length=50, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    profile_image = models.CharField(max_length=300)
+    # saved_plants = models.ManyToManyField(‘plants.Plant’, related_name=‘plants’)
---
+++ backend/jwt_auth/views.py
@@ -0,0 +1,7 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+
+from django.shortcuts import render
+
+# Create your views here.
