ec7953a7c91da804ec4ef45c32621d5c86d80b38
awyssa <michael.ehson@gmail.com>
2021-04-15 16:35:09 +0100

    added routes

--- backend/jwt_auth/serializers/populated.py
+++
@@ -1,6 +0,0 @@
-from jwt_auth.serializers.common import UserSerializer
-from ..serializers.common import PlantSerializer
-
-class PopulatedUserSerializer(UserSerializer):
-
-    plants = PlantsSerializer()
