d0c3646d3c3d3b78781d939b2c0cd7721c87f3cc
awyssa <michael.ehson@gmail.com>
2021-04-15 17:31:27 +0100

    frontend running;

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,39 +1,32 @@
 import React, { useEffect, useState } from 'react'
-// import axios from 'axios'
+import axios from 'axios'
 
 export const Home = () => {
   const [plantData, setPlantData] = useState(null)
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const resp = await fetch('/api/plants')
-        const json = await resp.json()
-        if (json.sucess) {
-          setPlantData(json.plants)
-        }
-      } catch (error) {
-        console.log('error', error)
-      }
+    const getData = async () => {
+      const response = await axios.get('/api/plants/')
+      setPlantData(response.data)
+      console.log('clog1', response.data)
     }
-    fetchData()
-    console.log(plantData)
+    getData()
+    console.log('clog2', plantData)
   }, [])
-
-  // useEffect(() => {
-  //   const getData = async () => {
-  //     const response = await axios.get('/api/plants')
-  //     setPlantData(response.data)
-  //     console.log('clog1', response.data)
-  //   }
-  //   getData()
-  //   console.log('clog2', plantData)
-  // }, [])
 
   return (
     <div>
       <p> welcome to the home page</p>
-      {/* <p>{plantData.id}</p> */}
+      {!plantData
+        ? <p> loading... </p>
+        : <div>
+          <p>{plantData[0].id}</p>
+        <p>{plantData[0].name}</p>
+        <p>{plantData[0].image}</p>
+        <p>{plantData[0].sunlight}</p>
+        <p>{plantData[0].owner.username}</p>
+        </div>
+      }
     </div>
   )
 }
