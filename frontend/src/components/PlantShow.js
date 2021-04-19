c11239e601edb7593988f52554c48fff432b8d16
Patrick Johnson <paddyjohnsonmusic@gmail.com>
2021-04-20 00:14:18 +0100

    info on show page, just needs styling

--- frontend/src/components/PlantShow.js
+++ frontend/src/components/PlantShow.js
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useHistory } from 'react-router-dom'
 import axios from 'axios'
 import { getTokenFromLocalStorage } from '../helpers/auth'
 import PlantWishList from './PlantWishList'
+import { Button } from 'react-bootstrap'
 
 const PlantShow = () => {
   const [onePlant, setOnePlant] = useState(null)
   const [userData, setUserData] = useState(null)
-  // const [wishlist, setUpdateWishlist] = useState({
-  //   saved_plants: []
-  // })
+  const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
   const params = useParams()
+  const history = useHistory()
   useEffect(() => {
     const getOnePlant = async () => {
       const response = await axios.get(`/api/plants/${params.id}`)
@@ -29,24 +29,30 @@ const PlantShow = () => {
     }
     getUser()
   }, [])
-  // const addToWishlist = (event) => {
-  //   // console.log('event.target.value', params.id)
-  //   const filteredWishlistConst = userData.saved_plants.push(params.id)
-  //   // // wishlist.push(params.id)
-  //   // console.log('wishlist', wishlist)
-  //   // console.log('filtered wishlist ', filteredWishlistConst)
-  //   const newWishList = { saved_plants: filteredWishlistConst }
-  //   // setUpdateWishlist(newWishList)
-  //   setUpdateWishlist(newWishList)
-  //   console.log(wishlist)
-  //   submit(wishlist)
-  // }
+  const handleClick = () => {
+    history.push('/home')
+  }
   if (!onePlant) return null
   return (
     <>
     <h2>{onePlant.name}</h2>
     <h3>{onePlant.type}</h3>
     <h3 className="science">{onePlant.subspecies}</h3>
+    <img src={onePlant.image} />
+    <p>{onePlant.description}</p>
+    <p>Sow in: {monthNames[onePlant.sow_month]}</p>
+    <p>Plant out in: {monthNames[onePlant.plant_month]}</p>
+    <p>Harvest in: {monthNames[onePlant.harvest_month]}</p>
+    <p>Sunlight requirements: {onePlant.sunlight}</p>
+    <p>Soil type: {onePlant.soil_acidity}</p>
+    <p>Water every {onePlant.watering_frequency} days</p>
+    <p>Fertilize every {onePlant.fertilizing_frequency} days with {onePlant.fertilizer_type}</p>
+    <p>Ideal germination temperature: {onePlant.germination_temperature}°C</p>
+    <p>Difficulty rating: {onePlant.difficulty}/5</p>
+    {onePlant.verified_by_admin === false
+      ? <p>Not yet verified by CornHub</p>
+      : <p>Verified by CornHub</p>
+    }
     {!userData
       ? <h1>Log in to save to profile!</h1>
       : <PlantWishList
@@ -54,6 +60,9 @@ const PlantShow = () => {
         plantId={params.id}
        />
 }
+<Button variant="primary" size="sm" onClick={handleClick}>
+      Back to Plants!
+    </Button>
     </>
   )
 }
