688cb70e623105d8c2640e0eb13d4940b95559ba
Patrick Johnson <paddyjohnsonmusic@gmail.com>
2021-04-20 22:38:23 +0100

    images in resources now fastlinks

--- frontend/src/components/Resources.js
+++ frontend/src/components/Resources.js
@@ -5,22 +5,33 @@ const Resources = () => {
   return (
     <Container className="resources-page">
       <Container className="spotify">
-        <p>CornHub Spotify playlist</p>
+        <p>CornHub Spotify Playlist</p>
         <iframe src="https://open.spotify.com/embed/playlist/3B73vSUPTr7RljzlpF1wcV" width="400" height="500" allowtransparency="true" allow="encrypted-media"></iframe>
         <img src="https://res.cloudinary.com/cornhub/image/upload/v1618923742/About/mushroom_cwmuus.webp" />
       </Container>
         <Container className="res-info">
+          <Container className="resources-item">
+          <p>Some of our Favourite Links</p>
+          </Container>
         <Container className="resources-item">
+        <a href="https://www.southdevonchillifarm.co.uk/" target="blank" >
         <img src="https://www.southdevonchillifarm.co.uk/images/logos/1/logo.png"/>
-        <a href="https://www.southdevonchillifarm.co.uk/" target="blank" >South Devon Chilli Farm </a>
+        </a>
         </Container>
         <Container className="resources-item">
+        <a href="https://theplantwarehouse.co.uk/" target="blank" >
         <img src="https://cdn.shopify.com/s/files/1/0379/6015/5267/files/Plants_House_Logo-01_x100.png?v=1588625584"/>
-        <a href="https://theplantwarehouse.co.uk/" target="blank" >Plant Warehouse London</a>
+        </a>
         </Container>
         <Container className="resources-item">
+        <a href="https://n1gardencentre.co.uk/" target="blank" >
         <img src="https://n1gardencentre.co.uk/wp-content/uploads/2019/02/logo-3.png"/>
-        <a href="https://n1gardencentre.co.uk/" target="blank" >N1 Garden Center</a>
+        </a>
+        </Container>
+        <Container className="resources-item">
+        <a href="https://www.otterfarm.co.uk/" target="blank" >
+        <img src="https://www.otterfarm.co.uk/wp-content/uploads/2016/10/otterfarmlogo-grey.png"/>
+        </a>
         </Container>
       </Container>
     </Container>
